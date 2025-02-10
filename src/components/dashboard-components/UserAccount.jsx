fbf3a754260f9b428ed42f24267d2fed5d358e28
habyarimana caleb <callebhabyar55@gmail.com>
2025-02-10 22:54:21 +0200

    update user in local storage by adding username to update status of current user account

--- src/components/dashboard-components/UserAccount.jsx
+++ src/components/dashboard-components/UserAccount.jsx
@@ -4,29 +4,31 @@ const UserAccount = ({ isSidebarExpanded }) => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    // Retrieve user data from localStorage
-    const storedUser = localStorage.getItem("user");
-    if (storedUser) {
-      setUser(JSON.parse(storedUser));
-    }
-  }, []); // Runs once when the component mounts
-
+    const getUserData = () => {
+      const storedUser = localStorage.getItem("user");
+      if (storedUser) {
+        setUser(JSON.parse(storedUser));
+      }
+    };
+    getUserData();
+    window.addEventListener("storage", getUserData);
+    return () => {
+      window.removeEventListener("storage", getUserData);
+    };
+  }, []);
   return (
     <div className="flex text-white py-4 px-2 mt-4 mb-2">
-      {/* User Profile */}
       <div className="relative">
         <img
           src="images/person-image-2.png"
           alt="User profile"
           className="w-8 h-8 rounded-full border-2 border-white"
         />
-        {/* Green Circle for User Status */}
         <div className="absolute bottom-0 right-0 w-3 h-3 bg-green-500 border-2 border-white rounded-full"></div>
       </div>
-
       {isSidebarExpanded && user && (
         <div className="flex flex-col text-justify ml-2">
-          <span className="text-xs font-semibold">{user.name}</span>
+          <span className="text-xs font-semibold">{user.userName}</span>
           <span className="text-xs">{user.email}</span>
         </div>
       )}
